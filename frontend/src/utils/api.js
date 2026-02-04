6222cd511cf13d28a69766f3307a0894fc7630a9
Kunjan Shah <kunjanone@gmail.com>
2026-02-04 20:44:31 +0100

    Fix API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 //const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
-const API_URL = 'https://llm-prompts-backend.onrender.com/api';
+//const API_URL = 'https://llm-prompts-backend.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://llm-prompts-backend.onrender.com';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
